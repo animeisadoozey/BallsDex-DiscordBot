18daa0619dcfff23c228e6882b56e44d50e6d696
Valen7440 <jijijija059@gmail.com>
2026-06-23 14:24:15 -0300

    fix the fix

--- ballsdex/packages/balls_extension/cog.py
+++ ballsdex/packages/balls_extension/cog.py
@@ -4,7 +4,11 @@ from tortoise.functions import Count
 
 from ballsdex.core.models import BallInstance, Player
 from ballsdex.core.utils.buttons import ConfirmChoiceView
-from ballsdex.core.utils.transformers import BallEnabledTransform, BallInstanceTransform, SpecialEnabledTransform
+from ballsdex.core.utils.transformers import (
+    BallEnabledTransform,
+    BallInstanceTransform,
+    SpecialEnabledTransform,
+)
 from ballsdex.packages.countryballs.countryball import BallSpawnView
 from ballsdex.settings import settings
 
@@ -13,9 +17,12 @@ if TYPE_CHECKING:
 
 medals = {1: "🥇", 2: "🥈", 3: "🥉"}
 
+
 @app_commands.command()
 @app_commands.checks.cooldown(1, 240, key=lambda i: i.user.id)
-async def drop(interaction: discord.Interaction["BallsDexBot"], countryball: BallInstanceTransform):
+async def drop(
+    interaction: discord.Interaction["BallsDexBot"], countryball: BallInstanceTransform
+):
     """
     Drop a countryball from your inventory.
 
@@ -26,22 +33,24 @@ async def drop(interaction: discord.Interaction["BallsDexBot"], countryball: Bal
     """
     channel = interaction.channel
     if not isinstance(channel, discord.TextChannel):
-        await interaction.response.send_message("This channel isn't a text channel.", ephemeral=True)
+        await interaction.response.send_message(
+            "This channel isn't a text channel.", ephemeral=True
+        )
         return
-    if not countryball.countryball.tradeable:
-        await interaction.response.send_message("The countryball must be tradeable.", ephemeral=True)
+    if not countryball.is_tradeable:
+        await interaction.response.send_message(
+            "The countryball must be tradeable.", ephemeral=True
+        )
         return
     await interaction.response.defer(thinking=True, ephemeral=True)
     description = countryball.description(include_emoji=True, bot=interaction.client)
     view = ConfirmChoiceView(
         interaction,
         accept_message=f"Confirmed, dropping {description}...",
-        cancel_message="Request cancelled."
+        cancel_message="Request cancelled.",
     )
     await interaction.followup.send(
-        f"Are you sure you want to drop {description}?",
-        view=view,
-        ephemeral=True
+        f"Are you sure you want to drop {description}?", view=view, ephemeral=True
     )
     await view.wait()
     if not view.value:
@@ -51,6 +60,7 @@ async def drop(interaction: discord.Interaction["BallsDexBot"], countryball: Bal
 
     ball_view = await BallSpawnView.from_existing(interaction.client, countryball)
     await ball_view.spawn(channel)
+
 
 @app_commands.command()
 async def leaderboard(
@@ -75,8 +85,7 @@ async def leaderboard(
         filters["ball"] = countryball
 
     query = (
-        BallInstance
-        .filter(**filters)
+        BallInstance.filter(**filters)
         .group_by("player_id")
         .annotate(ball_count=Count("id"))
         .order_by("-ball_count")
@@ -92,10 +101,7 @@ async def leaderboard(
     players_qs = await Player.filter(id__in=player_ids)
     players = {p.pk: p for p in players_qs}
     instances = [
-        {
-            "player": players[x["player_id"]],
-            "ball_count": x["ball_count"]
-        }
+        {"player": players[x["player_id"]], "ball_count": x["ball_count"]}
         for x in values
     ]
 
@@ -111,7 +117,11 @@ async def leaderboard(
         medal = medals.get(i, i)
 
         count = instance["ball_count"]
-        grammar = settings.collectible_name if count == 1 else settings.plural_collectible_name
+        grammar = (
+            settings.collectible_name
+            if count == 1
+            else settings.plural_collectible_name
+        )
         text += f"{medal}. {user.display_name} - {grammar}: {count}\n"
 
     ball_txt = countryball.country if countryball else ""
@@ -129,6 +139,7 @@ async def leaderboard(
     embed.set_thumbnail(url=interaction.user.display_avatar.url)
 
     await interaction.followup.send(embed=embed)
+
 
 # add all app_commands to this list, because it'll use in the startup
 commands: list[app_commands.Command] = [drop, leaderboard]
